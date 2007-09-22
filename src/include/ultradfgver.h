1f89e7c9066b54d0946da29375c49d98ca2f0965
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2007-09-22 12:01:21 +0000

    Website contents added. Scheduler batch build added.

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,5 +1,5 @@
-#define VERSION 1,1,1,0 
-#define VERSION2 "1, 1, 1, 0\0" 
-#define VERSIONINTITLE "UltraDefrag v1.1.1" 
-#define VERSIONINTITLE_U L"UltraDefrag v1.1.1" 
-#define ABOUT_VERSION "About UltraDefrag v1.1.1" 
+#define VERSION 1,2,0,0 
+#define VERSION2 "1, 2, 0, 0\0" 
+#define VERSIONINTITLE "UltraDefrag v1.2.0" 
+#define VERSIONINTITLE_U L"UltraDefrag v1.2.0" 
+#define ABOUT_VERSION "About UltraDefrag v1.2.0" 
