46d761414961f7c8e106376df8729a6166ed3d3f
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2008-03-09 12:44:20 +0000

    Version number changed to 1.3.4

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,4 +1,4 @@
-#define VERSION 1,3,3,0 
-#define VERSION2 "1, 3, 3, 0\0" 
-#define VERSIONINTITLE "UltraDefrag v1.3.3" 
-#define ABOUT_VERSION "About UltraDefrag v1.3.3" 
+#define VERSION 1,3,4,0 
+#define VERSION2 "1, 3, 4, 0\0" 
+#define VERSIONINTITLE "UltraDefrag v1.3.4" 
+#define ABOUT_VERSION "About UltraDefrag v1.3.4" 
