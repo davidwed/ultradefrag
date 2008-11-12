6f431a979b854c373a369f05b29c5ea5238a455f
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2008-11-12 09:42:36 +0000

    Version information updated.

--- src/dll/zenwinx/zenwinxver.h
+++ src/dll/zenwinx/zenwinxver.h
@@ -1,2 +1,2 @@
-#define ZENWINX_VERSION 0,0,2,0 
-#define ZENWINX_VERSION2 "0, 0, 2, 0\0" 
+#define ZENWINX_VERSION 0,1,0,0 
+#define ZENWINX_VERSION2 "0, 1, 0, 0\0" 
--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,4 +1,4 @@
-#define VERSION 1,4,1,0 
-#define VERSION2 "1, 4, 1, 0\0" 
-#define VERSIONINTITLE "UltraDefrag v1.4.1" 
-#define ABOUT_VERSION "Ultra Defragmenter version 1.4.1" 
+#define VERSION 2,0,0,0 
+#define VERSION2 "2, 0, 0, 0\0" 
+#define VERSIONINTITLE "UltraDefrag v2.0.0" 
+#define ABOUT_VERSION "Ultra Defragmenter version 2.0.0" 
