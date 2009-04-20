30409636b8c50fd8711229cf28844a8c6cb48077
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2009-04-20 11:56:03 +0000

    Manifests updated.

--- src/make-manifests.cmd
+++ src/make-manifests.cmd
@@ -35,12 +35,34 @@ type manifest.part3 >> .\obj\udefrag-gui-config\res\config.manifest
 echo processorArchitecture="%1" >> .\obj\udefrag-gui-config\res\config.manifest
 type manifest.part4 >> .\obj\udefrag-gui-config\res\config.manifest
 
+type manifest.part1 > .\obj\bootexctrl\bootexctrl.manifest
+echo version="%ULTRADFGVER%.0" name="bootexctrl" processorArchitecture="%1" >> .\obj\bootexctrl\bootexctrl.manifest
+type manifest.part2 >> .\obj\bootexctrl\bootexctrl.manifest
+echo BootExecute Control Program >> .\obj\bootexctrl\bootexctrl.manifest
+type manifest.part3 >> .\obj\bootexctrl\bootexctrl.manifest
+echo processorArchitecture="%1" >> .\obj\bootexctrl\bootexctrl.manifest
+type manifest.part4 >> .\obj\bootexctrl\bootexctrl.manifest
+
+type manifest.part1 > .\obj\lua5.1\lua.manifest
+echo version="5.1.2.0" name="Lua" processorArchitecture="%1" >> .\obj\lua5.1\lua.manifest
+type manifest.part2 >> .\obj\lua5.1\lua.manifest
+echo Lua Console >> .\obj\lua5.1\lua.manifest
+type manifest.part3 >> .\obj\lua5.1\lua.manifest
+echo processorArchitecture="%1" >> .\obj\lua5.1\lua.manifest
+type manifest.part4a >> .\obj\lua5.1\lua.manifest
+echo processorArchitecture="%1" >> .\obj\lua5.1\lua.manifest
+type manifest.part5 >> .\obj\lua5.1\lua.manifest
+
 rem update manifests in working copy of sources
+if "%1" neq "X86" goto L1
 copy /Y .\obj\console\defrag.manifest .\console\defrag.manifest
 copy /Y .\obj\gui\res\dfrg.manifest .\gui\res\dfrg.manifest
 copy /Y .\obj\gui-launcher\udefrag-gui.manifest .\gui-launcher\udefrag-gui.manifest
 copy /Y .\obj\udefrag-gui-config\res\config.manifest .\udefrag-gui-config\res\config.manifest
+copy /Y .\obj\bootexctrl\bootexctrl.manifest .\bootexctrl\bootexctrl.manifest
+copy /Y .\obj\lua5.1\lua.manifest .\lua5.1\lua.manifest
 
+:L1
 exit /B 0
 
 :failure
