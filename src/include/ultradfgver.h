c2704fd98f0d3220f7e02dc9cb5c23f02ad2da9f
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2009-07-02 06:50:39 +0000

    Version information updated.

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,4 +1,4 @@
-#define VERSION 3,1,1,0 
-#define VERSION2 "3, 1, 1, 0\0" 
-#define VERSIONINTITLE "UltraDefrag v3.1.1" 
-#define ABOUT_VERSION "Ultra Defragmenter version 3.1.1" 
+#define VERSION 3,2,0,0 
+#define VERSION2 "3, 2, 0, 0\0" 
+#define VERSIONINTITLE "UltraDefrag v3.2.0" 
+#define ABOUT_VERSION "Ultra Defragmenter version 3.2.0" 
