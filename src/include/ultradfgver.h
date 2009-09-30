29923aa58212639c8903283da038018800df44c6
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2009-09-30 11:25:04 +0000

    Version number changed to 3.2.1

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,4 +1,4 @@
-#define VERSION 3,2,0,0 
-#define VERSION2 "3, 2, 0, 0\0" 
-#define VERSIONINTITLE "UltraDefrag v3.2.0" 
-#define ABOUT_VERSION "Ultra Defragmenter version 3.2.0" 
+#define VERSION 3,2,1,0 
+#define VERSION2 "3, 2, 1, 0\0" 
+#define VERSIONINTITLE "UltraDefrag v3.2.1" 
+#define ABOUT_VERSION "Ultra Defragmenter version 3.2.1" 
