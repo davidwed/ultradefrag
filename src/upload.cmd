575f72406ee67d77b8cd98502357e2d1b6427740
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2009-11-30 15:41:27 +0000

    Few little bugs were fixed.

--- src/upload.cmd
+++ src/upload.cmd
@@ -3,25 +3,31 @@
 rem This script was made for myself (Dmitri Arkhangelski)
 rem to simplify binary packages uploading.
 
-call build.cmd
-call build-micro.cmd
-
 mkdir archive
+
+call build.cmd
 copy .\bin\ultradefrag-%ULTRADFGVER%.bin.i386.exe .\archive\
-copy .\bin\amd64\ultradefrag-%ULTRADFGVER%.bin.amd64.exe .\archive\
-copy .\bin\ia64\ultradefrag-%ULTRADFGVER%.bin.ia64.exe .\archive\
-
-copy .\bin\ultradefrag-micro-edition-%ULTRADFGVER%.bin.i386.exe .\archive\
-copy .\bin\amd64\ultradefrag-micro-edition-%ULTRADFGVER%.bin.amd64.exe .\archive\
-copy .\bin\ia64\ultradefrag-micro-edition-%ULTRADFGVER%.bin.ia64.exe .\archive\
-
 copy .\src_package\ultradefrag-%ULTRADFGVER%.src.7z .\archive\
+
+call build-micro.cmd
+copy .\bin\ultradefrag-micro-edition-%ULTRADFGVER%.bin.i386.exe .\archive\
+
+call build-portable.cmd
+copy .\bin\ultradefrag-portable-%ULTRADFGVER%.bin.zip .\archive\
+
+call build-micro-portable.cmd
+copy .\bin\ultradefrag-micro-portable-%ULTRADFGVER%.bin.zip .\archive\
 
 cd archive
 
 md5sum ultradefrag-%ULTRADFGVER%.bin.* > ultradefrag-%ULTRADFGVER%.MD5SUMS
 md5sum ultradefrag-micro-edition-%ULTRADFGVER%.bin.* >> ultradefrag-%ULTRADFGVER%.MD5SUMS
+md5sum ultradefrag-portable-%ULTRADFGVER%.bin.* >> ultradefrag-%ULTRADFGVER%.MD5SUMS
+md5sum ultradefrag-micro-portable-%ULTRADFGVER%.bin.* >> ultradefrag-%ULTRADFGVER%.MD5SUMS
 md5sum ultradefrag-%ULTRADFGVER%.src.* >> ultradefrag-%ULTRADFGVER%.MD5SUMS
+
+cd ..
+exit /B 0
 
 echo cd /incoming/d/dm/dmitriar/uploads > update
 echo put ultradefrag-%ULTRADFGVER%.bin.amd64.exe >> update
