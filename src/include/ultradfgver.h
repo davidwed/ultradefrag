14fd19fb0eac074eba0518facfbbfb86e904375b
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-02-07 16:55:29 +0000

    Sources prepared for the next release.

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,5 +1,5 @@
-#define VERSION 4,0,0,0 
-#define VERSION2 "4, 0, 0, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 4.0.0" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 4.0.0 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 4.0.0" 
+#define VERSION 4,0,1,0 
+#define VERSION2 "4, 0, 1, 0\0" 
+#define VERSIONINTITLE "UltraDefrag 4.0.1" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 4.0.1 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 4.0.1" 
