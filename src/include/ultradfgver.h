810e0f6c8b88752445c8c699d0610afb266750bb
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-03-07 18:12:47 +0000

    Version number changed to 4.2.0.

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,5 +1,5 @@
-#define VERSION 4,1,0,0 
-#define VERSION2 "4, 1, 0, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 4.1.0" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 4.1.0 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 4.1.0" 
+#define VERSION 4,2,0,0 
+#define VERSION2 "4, 2, 0, 0\0" 
+#define VERSIONINTITLE "UltraDefrag 4.2.0" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 4.2.0 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 4.2.0" 
