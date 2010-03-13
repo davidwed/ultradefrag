410ae62014e7a254fc037a205e2a4082ac979552
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-03-13 23:42:28 +0000

    added doxy-doc folders created by build-doc.cmd to cleanup.cmd

--- src/cleanup.cmd
+++ src/cleanup.cmd
@@ -10,6 +10,12 @@ rd /s /q bin
 rd /s /q lib
 rd /s /q obj
 rd /s /q doxy-doc
+rd /s /q driver\doxy-doc
+rd /s /q dll\wgx\doxy-doc
+rd /s /q dll\udefrag\doxy-doc
+rd /s /q dll\zenwinx\doxy-doc
+rd /s /q dll\udefrag-kernel\doxy-doc
+rd /s /q ..\doc\html\handbook\doxy-doc
 rd /s /q src_package
 rd /s /q pre-release
 
