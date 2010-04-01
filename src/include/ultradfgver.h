8689fd101fe178119dcbe537cbe6599a3e5d0902
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-04-01 08:30:00 +0000

    Version number changed to 4.3.0.

--- src/include/ultradfgver.h
+++ src/include/ultradfgver.h
@@ -1,5 +1,5 @@
-#define VERSION 4,2,0,0 
-#define VERSION2 "4, 2, 0, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 4.2.0" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 4.2.0 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 4.2.0" 
+#define VERSION 4,3,0,0 
+#define VERSION2 "4, 3, 0, 0\0" 
+#define VERSIONINTITLE "UltraDefrag 4.3.0" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 4.3.0 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 4.3.0" 
--- src/setvars.cmd
+++ src/setvars.cmd
@@ -1,11 +1,8 @@
 @echo off
 echo Set common environment variables...
-set VERSION=4,2,0,0
-set VERSION2="4, 2, 0, 0\0"
-set ZENWINX_VERSION=2,1,0,0
-set ZENWINX_VERSION2="2, 1, 0, 0\0"
-set ULTRADFGVER=4.2.0
-set ZENWINXVER=2.1.0
+set VERSION=4,3,0,0
+set VERSION2="4, 3, 0, 0\0"
+set ULTRADFGVER=4.3.0
 set WINDDKBASE=D:\WINDDK\3790~1.183
 set WINSDKBASE=C:\Program Files\Microsoft SDKs\Windows\v6.1
 set MINGWBASE=D:\Software\MinGWStudio\MinGW
