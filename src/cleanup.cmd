a297ff866d7abcab95afb384ea39498eae393579
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-04-25 20:52:18 +0000

    added cleanup of temporary source package folder do we need to keep the archive folder? do we need to create the lib and obj folder, which are created by build and build-micro?

--- src/cleanup.cmd
+++ src/cleanup.cmd
@@ -3,6 +3,8 @@
 rem This script performs UltraDefrag source directory cleanup
 rem by removing all intermediate files.
 
+call ParseCommandLine.cmd %*
+
 echo Delete all intermediate files...
 rem call SETVARS.CMD
 rem call BLDSCHED.CMD --clean
@@ -17,11 +19,19 @@ rd /s /q dll\zenwinx\doxy-doc
 rd /s /q dll\udefrag-kernel\doxy-doc
 rd /s /q ..\doc\html\handbook\doxy-doc
 rd /s /q src_package
+rd /s /q ..\src_package
 rd /s /q pre-release
 
-if "%1" equ "--clean" goto end
+rem do we need to keep the archive?
+rem rd /s /q archive
+
+if %UD_BLD_FLG_ONLY_CLEANUP% equ 1 goto end
 
 rem Recreate bin, obj, lib, doxy-doc directories
+rem
+rem obj and lib are created by build and build-micro
+rem so we could skip them here
+rem
 mkdir bin
 mkdir lib
 mkdir obj
