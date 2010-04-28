20aa5895d8243ff9dce685e6e466bf8d55e167c8
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-04-28 10:50:36 +0000

    changed to a more descriptive variable name

--- src/build-pre-release.cmd
+++ src/build-pre-release.cmd
@@ -4,9 +4,9 @@ rem This script was made for myself (Stefan Pendl)
 rem to simplify pre-release (RC) building.
 
 echo.
-set /P ULTRADFGRC="Enter RC Version number [1]: "
+set /P UD_BLD_PRE_RELEASE_VER="Enter RC Version number [1]: "
 
-if "%ULTRADFGRC%" == "" set ULTRADFGRC=1
+if "%UD_BLD_PRE_RELEASE_VER%" == "" set UD_BLD_PRE_RELEASE_VER=1
 
 set script_dir=%~dp0
 
@@ -18,13 +18,13 @@ cd /D %script_dir%
 
 mkdir pre-release
 echo.
-copy /b /y /v .\bin\ultradefrag-%ULTRADFGVER%.bin.i386.exe        .\pre-release\ultradefrag-%ULTRADFGVER%-RC%ULTRADFGRC%.bin.i386.exe
-copy /b /y /v .\bin\amd64\ultradefrag-%ULTRADFGVER%.bin.amd64.exe .\pre-release\ultradefrag-%ULTRADFGVER%-RC%ULTRADFGRC%.bin.amd64.exe
-copy /b /y /v .\bin\ia64\ultradefrag-%ULTRADFGVER%.bin.ia64.exe   .\pre-release\ultradefrag-%ULTRADFGVER%-RC%ULTRADFGRC%.bin.ia64.exe
+copy /b /y /v .\bin\ultradefrag-%ULTRADFGVER%.bin.i386.exe        .\pre-release\ultradefrag-%ULTRADFGVER%-RC%UD_BLD_PRE_RELEASE_VER%.bin.i386.exe
+copy /b /y /v .\bin\amd64\ultradefrag-%ULTRADFGVER%.bin.amd64.exe .\pre-release\ultradefrag-%ULTRADFGVER%-RC%UD_BLD_PRE_RELEASE_VER%.bin.amd64.exe
+copy /b /y /v .\bin\ia64\ultradefrag-%ULTRADFGVER%.bin.ia64.exe   .\pre-release\ultradefrag-%ULTRADFGVER%-RC%UD_BLD_PRE_RELEASE_VER%.bin.ia64.exe
 
 cd ..
 echo.
-echo RC%ULTRADFGRC% Pre-Release created successfully!
+echo RC%UD_BLD_PRE_RELEASE_VER% Pre-Release created successfully!
 exit /B 0
 
 :build_failed
