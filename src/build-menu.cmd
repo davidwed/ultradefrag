0a3cf18eac2fd7d2b29d080c96615d8404983678
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-06-18 08:17:17 +0000

    added window title

--- src/build-menu.cmd
+++ src/build-menu.cmd
@@ -6,6 +6,7 @@ rem it includes the most common build situations
 set UD_BLD_MENU_DIR=%~dp0
 
 :DisplayMenu
+title UltraDefrag Project - Build Menu
 cls
 echo.
 echo UltraDefrag Project - Build Menu
@@ -54,86 +55,109 @@ goto AskSelection
 goto :EOF
 
 :1
+title Clean Project Folder
 call cleanup.cmd --clean
 goto finished
 
 :2
+title Build Release Candidate
 call build-pre-release.cmd
 goto finished
 
 :3
+title Build Release
 call build-release.cmd
 goto finished
 
 :4
+title Build with Defaults
 call build.cmd
 goto finished
 
 :5
+title Build with Defaults and Install
 call build.cmd --install
 goto finished
 
 :6
+title Build .................. using WinDDK, no IA64
 call build.cmd --use-winddk --no-ia64
 goto finished
 
 :7
+title Build Portable ......... using WinDDK, no IA64
 call build-portable.cmd --use-winddk --no-ia64
 goto finished
 
 :8
+title Build Micro ............ using WinDDK, no IA64
 call build-micro.cmd --use-winddk --no-ia64
 goto finished
 
 :9
+title Build Micro Portable ... using WinDDK, no IA64
 call build-micro-portable.cmd --use-winddk --no-ia64
 goto finished
 
 :10
+title Build Docs
 call build-docs.cmd
 goto finished
 
 :11
+title Build .................. with Custom Switches
 cls
 echo.
 call build.cmd --help
 echo.
 set /p UD_BLD_MENU_SWITCH="Please enter Switches: "
+
+title Build .................. %UD_BLD_MENU_SWITCH%
 echo.
 call build.cmd %UD_BLD_MENU_SWITCH%
 goto finished
 
 :12
+title Build Portable ......... with Custom Switches
 cls
 echo.
 call build.cmd --help
 echo.
 set /p UD_BLD_MENU_SWITCH="Please enter Switches: "
+
+title Build Portable ......... %UD_BLD_MENU_SWITCH%
 echo.
 call build-portable.cmd %UD_BLD_MENU_SWITCH%
 goto finished
 
 :13
+title Build Micro ............ with Custom Switches
 cls
 echo.
-call build-micro.cmd --help
+call build.cmd --help
 echo.
 set /p UD_BLD_MENU_SWITCH="Please enter Switches: "
+
+title Build Micro ............ %UD_BLD_MENU_SWITCH%
 echo.
 call build-micro.cmd %UD_BLD_MENU_SWITCH%
 goto finished
 
 :14
+title Build Micro Portable ... with Custom Switches
 cls
 echo.
-call build-micro.cmd --help
+call build.cmd --help
 echo.
 set /p UD_BLD_MENU_SWITCH="Please enter Switches: "
+
+title Build Micro Portable ... %UD_BLD_MENU_SWITCH%
 echo.
 call build-micro-portable.cmd %UD_BLD_MENU_SWITCH%
 goto finished
 
 :15
+title Build Test Release for Stefan
 call build-pre-release.cmd --no-ia64 --install
 
 if not exist "%USERPROFILE%\Downloads\UltraDefrag" mkdir "%USERPROFILE%\Downloads\UltraDefrag"
@@ -142,6 +166,7 @@ copy /b /y /v %UD_BLD_MENU_DIR%\pre-release\*.* "%USERPROFILE%\Downloads\UltraDe
 goto finished
 
 :16
+title Build Test Installation for Stefan
 call build-pre-release.cmd --no-ia64 --no-x86 --install
 
 goto finished
