3496192ebd7bb58892bc5bf5e283a7a3ed56612e
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-07-01 07:06:44 +0000

    if a driver for an USB keyboard is not yet initialized, the same error message is returned, so the delay is only skipped on Win2k and below

--- src/dll/zenwinx/keyboard.c
+++ src/dll/zenwinx/keyboard.c
@@ -188,7 +188,7 @@ int __stdcall kb_open_internal(int device_number)
 				device_name,(UINT)Status);
 			winx_printf("%s\n",winx_get_error_description((ULONG)Status));
             
-            if (Status == STATUS_OBJECT_NAME_NOT_FOUND) return (-2);
+            if (Status == STATUS_OBJECT_NAME_NOT_FOUND && winx_get_os_version() < 51) return (-2);
 		}
 		return (-1);
 	}
