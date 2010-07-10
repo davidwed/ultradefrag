e4c5949c5380feaded4f77c1c9fb9129ebffd9a5
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-07-10 08:22:03 +0000

    added script to create virtual test disks for VirtualBox

---
+++ TestSuite/VirtualBox Create Test Hardisks.cmd
@@ -0,0 +1,82 @@
+@echo off
+::
+:: This script creates virtual harddisks
+::
+:: It can be used to create {Prefix}FAT.vdi, {Prefix}NTFS.vdi, {Prefix}UDF.vdi
+::
+
+:: installation folder of VirtualBox
+set VBoxRoot=%ProgramFiles%\Oracle\VirtualBox
+
+:: Folder containing the virtual harddisks
+set HDRoot=Z:\VirtualBox\HardDisks
+
+if not exist "%VBoxRoot%" goto :noVBroot
+if not exist "%HDRoot%" goto :noHDroot
+
+echo.
+set /p prefix="Enter Prefix (Example: XPTest): "
+echo.
+set /p createFAT="Create FAT Disks ([Y]/N)? "
+echo.
+set /p createNTFS="Create NTFS Disks ([Y]/N)? "
+echo.
+set /p createUDF="Create UDF Disks ([Y]/N)? "
+
+if "%prefix%" == "" goto :quit
+
+cd /d %VBoxRoot%
+
+:FAT
+if /i "%createFAT%" == "N" goto :NTFS
+
+for %%F in ( FAT FAT32 ) do (
+    echo.
+    echo ---------------------------------------
+    echo.
+    echo Creating "%HDRoot%\%prefix%%%F.vdi" ...
+    echo.
+    VBoxManage createhd --filename "%HDRoot%\%prefix%%%F.vdi" --size 1024 --remember
+)
+
+:NTFS
+if /i "%createNTFS%" == "N" goto :UDF
+
+for %%F in ( NTFS NTFScompressed NTFSmixed ) do (
+    echo.
+    echo ---------------------------------------
+    echo.
+    echo Creating "%HDRoot%\%prefix%%%F.vdi" ...
+    echo.
+    VBoxManage createhd --filename "%HDRoot%\%prefix%%%F.vdi" --size 1024 --remember
+)
+
+:UDF
+if /i "%createUDF%" == "N" goto :quit
+
+for %%F in ( UDF102 UDF150 UDF200 UDF201 UDF250 UDF250mirror ) do (
+    echo.
+    echo ---------------------------------------
+    echo.
+    echo Creating "%HDRoot%\%prefix%%%F.vdi" ...
+    echo.
+    VBoxManage createhd --filename "%HDRoot%\%prefix%%%F.vdi" --size 1024 --remember
+)
+
+:quit
+echo.
+echo ---------------------------------------
+echo.
+pause
+
+goto :EOF
+
+:noVBroot
+echo.
+echo VirtualBox not installed, aborting ...
+goto :quit
+
+:noHDroot
+echo.
+echo Harddisk Root Folder missing, aborting ...
+goto :quit
