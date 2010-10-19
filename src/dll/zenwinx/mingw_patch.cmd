925d93dddcd57f6fd2dd2876df10e2a92c0f4610
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-10-19 19:55:20 +0000

    cleanup

--- src/dll/zenwinx/mingw_patch.cmd
+++ src/dll/zenwinx/mingw_patch.cmd
@@ -5,24 +5,32 @@ echo with more adequate versions.
 echo Usage: mingw_patch.cmd {path to mingw installation}
 echo.
 
-if "%1" equ "" goto the_end
+if "%1" equ "" (
+	exit /B 0
+)
+
 echo Preparing to patch applying...
 set path=%1\bin;%path%
-if exist %1\lib\libntdll.a.orig goto ntdll_saved
-move %1\lib\libntdll.a %1\lib\libntdll.a.orig
-:ntdll_saved
-if exist %1\lib\libntoskrnl.a.orig goto ntoskrnl_saved
-move %1\lib\libntoskrnl.a %1\lib\libntoskrnl.a.orig
-:ntoskrnl_saved
+
+:: save original versions
+if not exist %1\lib\libntdll.a.orig (
+	move %1\lib\libntdll.a %1\lib\libntdll.a.orig
+)
+if not exist %1\lib\libntoskrnl.a.orig (
+	move %1\lib\libntoskrnl.a %1\lib\libntoskrnl.a.orig
+)
+
+:: generate more adequate versions
 dlltool -k --output-lib libntdll.a --def ntdll.def
 if %errorlevel% neq 0 goto fail
 dlltool -k --output-lib libntoskrnl.a --def ntoskrnl.def
 if %errorlevel% neq 0 goto fail
+
+:: replace files
 move /Y libntdll.a %1\lib\libntdll.a
 move /Y libntoskrnl.a %1\lib\libntoskrnl.a
 
 echo MinGW patched successfully!
-:the_end
 exit /B 0
 
 :fail
--- src/mingw_patch.cmd
+++ src/mingw_patch.cmd
@@ -1,16 +1,14 @@
 @echo off
 
 cd dll\zenwinx
-if %errorlevel% neq 0 goto fail
+if %errorlevel% neq 0 (
+	exit /B 1
+)
 
 call mingw_patch.cmd %1
-if %errorlevel% equ 0 goto the_end
-cd ..\..
-goto fail
-
-:the_end
+if %errorlevel% neq 0 (
+	cd ..\..
+	exit /B 1
+)
 cd ..\..
 exit /B 0
-
-:fail
-exit /B 1
