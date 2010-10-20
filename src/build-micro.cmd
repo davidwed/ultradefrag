88d80c2c9dad313dd55df96c37e2315bb99aa384
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2010-10-20 18:36:36 +0000

    cleanup

--- src/build-micro.cmd
+++ src/build-micro.cmd
@@ -1,21 +1,28 @@
 @echo off
-
-echo Build script for the UltraDefrag Micro Edition project.
-echo Copyright (c) 2007-2010 by Dmitri Arkhangelski (dmitriar@gmail.com).
+::
+:: Build script for UltraDefrag Micro Edition.
+::
 
 call ParseCommandLine.cmd %*
 
-if %UD_BLD_FLG_DIPLAY_HELP% equ 1 goto usage
+if %UD_BLD_FLG_DIPLAY_HELP% equ 1 (
+	call build-help.cmd "%~n0"
+	exit /B 0
+)
 
 set UD_MICRO_EDITION=1
 
-echo Set environment variables...
+:: set environment variables
 set OLD_PATH=%path%
 call SETVARS.CMD
 if exist "setvars_%COMPUTERNAME%_%ORIG_USERNAME%.cmd" call "setvars_%COMPUTERNAME%_%ORIG_USERNAME%.cmd"
 if exist "setvars_%COMPUTERNAME%_%USERNAME%.cmd" call "setvars_%COMPUTERNAME%_%USERNAME%.cmd"
 
-rem DELETE ALL PREVIOUSLY COMPILED FILES
+:: save current directory
+:: it will be restored on end/fail labels
+pushd .
+
+:: delete all previously compiled files
 call cleanup.cmd %*
 if %UD_BLD_FLG_ONLY_CLEANUP% equ 1 goto end
 
@@ -36,89 +43,71 @@ echo #define ZENWINX_VERSION %VERSION% > .\dll\zenwinx\zenwinxver.h
 echo #define ZENWINX_VERSION2 %VERSION2% >> .\dll\zenwinx\zenwinxver.h
 
 :: build all binaries
-call build-targets.cmd %*
-if %errorlevel% neq 0 goto fail
+call build-targets.cmd %* || goto fail
 
+:: in case of portable package there is nothing more to build
 if %UD_BLD_FLG_IS_PORTABLE% equ 1 goto end
 
-echo Build installer...
-cd .\bin
-
-:build_x86_installer
-if %UD_BLD_FLG_BUILD_X86% EQU 0 goto build_amd64_installer
-
-copy /Y ..\installer\MicroEdition.nsi .\
-
-"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=i386 MicroEdition.nsi
-if %errorlevel% neq 0 goto fail
-
-:build_amd64_installer
-if %UD_BLD_FLG_BUILD_AMD64% equ 0 goto build_ia64_installer
-
-copy /Y ..\installer\MicroEdition.nsi .\amd64\
-cd amd64
-"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=amd64 MicroEdition.nsi
-if %errorlevel% neq 0 goto fail
-cd..
-
-:build_ia64_installer
-if %UD_BLD_FLG_BUILD_IA64% equ 0 goto build_source_package
-
-copy /Y ..\installer\MicroEdition.nsi .\ia64\
-cd ia64
-"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=ia64 MicroEdition.nsi
-if %errorlevel% neq 0 goto fail
-cd ..
-
-:build_source_package
-rem This section was removed to increase compilation speed.
-rem Run build.cmd to make an archive of sources.
-cd ..
-
+echo Build installers...
+if %UD_BLD_FLG_BUILD_X86% neq 0 (
+	cd .\bin
+	copy /Y ..\installer\MicroEdition.nsi .\
+	"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=i386 MicroEdition.nsi || goto fail
+	cd ..
+)
+if %UD_BLD_FLG_BUILD_AMD64% neq 0 (
+	cd .\bin\amd64
+	copy /Y ..\..\installer\MicroEdition.nsi .\
+	"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=amd64 MicroEdition.nsi || goto fail
+	cd ..\..
+)
+if %UD_BLD_FLG_BUILD_IA64% neq 0 (
+	cd .\bin\ia64
+	copy /Y ..\..\installer\MicroEdition.nsi .\
+	"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=ia64 MicroEdition.nsi || goto fail
+	cd ..\..
+)
 echo.
 echo Build success!
 
+:: install the program if requested
 if %UD_BLD_FLG_DO_INSTALL% equ 0 goto end
 
-:install
 echo Start installer...
-if not %PROCESSOR_ARCHITECTURE% == x86 goto install_amd64
-.\bin\ultradefrag-micro-edition-%ULTRADFGVER%.bin.i386.exe /S
-if %errorlevel% neq 0 goto fail_inst
+if %PROCESSOR_ARCHITECTURE% == x86 (
+	.\bin\ultradefrag-micro-edition-%ULTRADFGVER%.bin.i386.exe /S || goto install_fail
+	goto install_success
+)
+if %PROCESSOR_ARCHITECTURE% == AMD64 (
+	.\bin\amd64\ultradefrag-micro-edition-%ULTRADFGVER%.bin.amd64.exe /S || goto install_fail
+	goto install_success
+)
+if %PROCESSOR_ARCHITECTURE% == IA64 (
+	.\bin\ia64\ultradefrag-micro-edition-%ULTRADFGVER%.bin.ia64.exe /S || goto install_fail
+	goto install_success
+)
 
-:install_amd64
-if not %PROCESSOR_ARCHITECTURE% == AMD64 goto install_ia64
-.\bin\amd64\ultradefrag-micro-edition-%ULTRADFGVER%.bin.amd64.exe /S
-if %errorlevel% neq 0 goto fail_inst
-
-:install_ia64
-if not %PROCESSOR_ARCHITECTURE% == IA64 goto install_finished
-.\bin\ia64\ultradefrag-micro-edition-%ULTRADFGVER%.bin.ia64.exe /S
-if %errorlevel% neq 0 goto fail_inst
-
-:install_finished
+:install_success
 echo Install success!
 goto end
 
-:fail_inst
+:install_fail
 echo Install error!
 goto end_fail
 
 :fail
-echo.
 echo Build error (code %ERRORLEVEL%)!
 
 :end_fail
 set UD_MICRO_EDITION=
 set Path=%OLD_PATH%
 set OLD_PATH=
+popd
 exit /B 1
 
 :end
 set UD_MICRO_EDITION=
 set Path=%OLD_PATH%
 set OLD_PATH=
+popd
 exit /B 0
-
-:usage
-call build-help.cmd "%~n0"
--- src/build.cmd
+++ src/build.cmd
@@ -1,19 +1,26 @@
 @echo off
-
-echo Build script for the UltraDefrag project.
-echo Copyright (c) 2007-2010 by Dmitri Arkhangelski (dmitriar@gmail.com).
+::
+:: Build script for the UltraDefrag project.
+::
 
 call ParseCommandLine.cmd %*
 
-if %UD_BLD_FLG_DIPLAY_HELP% equ 1 goto usage
+if %UD_BLD_FLG_DIPLAY_HELP% equ 1 (
+	call build-help.cmd "%~n0"
+	exit /B 0
+)
 
-echo Set environment variables...
+:: set environment variables
 set OLD_PATH=%path%
 call SETVARS.CMD
 if exist "setvars_%COMPUTERNAME%_%ORIG_USERNAME%.cmd" call "setvars_%COMPUTERNAME%_%ORIG_USERNAME%.cmd"
 if exist "setvars_%COMPUTERNAME%_%USERNAME%.cmd" call "setvars_%COMPUTERNAME%_%USERNAME%.cmd"
 
-rem DELETE ALL PREVIOUSLY COMPILED FILES
+:: save current directory
+:: it will be restored on end/fail labels
+pushd .
+
+:: delete all previously compiled files
 call cleanup.cmd %*
 if %UD_BLD_FLG_ONLY_CLEANUP% equ 1 goto end
 
@@ -36,83 +43,65 @@ echo #define ZENWINX_VERSION %VERSION% > .\dll\zenwinx\zenwinxver.h
 echo #define ZENWINX_VERSION2 %VERSION2% >> .\dll\zenwinx\zenwinxver.h
 
 :: build all binaries
-call build-targets.cmd %*
-if %errorlevel% neq 0 goto fail
+call build-targets.cmd %* || goto fail
 
-call build-docs.cmd
-if %errorlevel% neq 0 goto fail
+:: build documentation
+call build-docs.cmd || goto fail
 
+:: in case of portable package there is nothing more to build
 if %UD_BLD_FLG_IS_PORTABLE% equ 1 goto end
 
-echo Build installer...
-cd .\bin
-
-:build_x86_installer
-if %UD_BLD_FLG_BUILD_X86% EQU 0 goto build_amd64_installer
-
-copy /Y ..\installer\UltraDefrag.nsi .\
-copy /Y ..\installer\lang.ini .\
-copy /Y ..\installer\lang-classical.ini .\
-
-"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=i386 UltraDefrag.nsi
-if %errorlevel% neq 0 goto fail
-
-:build_amd64_installer
-if %UD_BLD_FLG_BUILD_AMD64% equ 0 goto build_ia64_installer
-
-copy /Y ..\installer\UltraDefrag.nsi .\amd64\
-copy /Y ..\installer\lang.ini .\amd64\
-copy /Y ..\installer\lang-classical.ini .\amd64\
-
-cd amd64
-
-"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=amd64 UltraDefrag.nsi
-if %errorlevel% neq 0 goto fail
-cd..
-
-:build_ia64_installer
-if %UD_BLD_FLG_BUILD_IA64% equ 0 goto build_source_package
-
-copy /Y ..\installer\UltraDefrag.nsi .\ia64\
-copy /Y ..\installer\lang.ini .\ia64\
-copy /Y ..\installer\lang-classical.ini .\ia64\
-
-cd ia64
-
-"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=ia64 UltraDefrag.nsi
-if %errorlevel% neq 0 goto fail
-cd..
-
-:build_source_package
-cd ..
-
-if %UD_BLD_FLG_IS_PRE_RELEASE% equ 1 goto install
-
+:: build installers
+echo Build installers...
+if %UD_BLD_FLG_BUILD_X86% neq 0 (
+	cd .\bin
+	copy /Y ..\installer\UltraDefrag.nsi .\
+	copy /Y ..\installer\lang.ini .\
+	copy /Y ..\installer\lang-classical.ini .\
+	"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=i386 UltraDefrag.nsi || goto fail
+	cd ..
+)
+if %UD_BLD_FLG_BUILD_AMD64% neq 0 (
+	cd .\bin\amd64
+	copy /Y ..\..\installer\UltraDefrag.nsi .\
+	copy /Y ..\..\installer\lang.ini .\
+	copy /Y ..\..\installer\lang-classical.ini .\
+	"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=amd64 UltraDefrag.nsi || goto fail
+	cd ..\..
+)
+if %UD_BLD_FLG_BUILD_IA64% neq 0 (
+	cd .\bin\ia64
+	copy /Y ..\..\installer\UltraDefrag.nsi .\
+	copy /Y ..\..\installer\lang.ini .\
+	copy /Y ..\..\installer\lang-classical.ini .\
+	"%NSISDIR%\makensis.exe" /DULTRADFGVER=%ULTRADFGVER% /DULTRADFGARCH=ia64 UltraDefrag.nsi || goto fail
+	cd ..\..
+)
+echo.
 echo Build success!
 
-:install
+:: install the program if requested
 if %UD_BLD_FLG_DO_INSTALL% equ 0 goto end
 
 echo Start installer...
-if not %PROCESSOR_ARCHITECTURE% == x86 goto install_amd64
-.\bin\ultradefrag-%ULTRADFGVER%.bin.i386.exe /S
-if %errorlevel% neq 0 goto fail_inst
+if %PROCESSOR_ARCHITECTURE% == x86 (
+	.\bin\ultradefrag-%ULTRADFGVER%.bin.i386.exe /S || goto install_fail
+	goto install_success
+)
+if %PROCESSOR_ARCHITECTURE% == AMD64 (
+	.\bin\amd64\ultradefrag-%ULTRADFGVER%.bin.amd64.exe /S || goto install_fail
+	goto install_success
+)
+if %PROCESSOR_ARCHITECTURE% == IA64 (
+	.\bin\ia64\ultradefrag-%ULTRADFGVER%.bin.ia64.exe /S || goto install_fail
+	goto install_success
+)
 
-:install_amd64
-if not %PROCESSOR_ARCHITECTURE% == AMD64 goto install_ia64
-.\bin\amd64\ultradefrag-%ULTRADFGVER%.bin.amd64.exe /S
-if %errorlevel% neq 0 goto fail_inst
-
-:install_ia64
-if not %PROCESSOR_ARCHITECTURE% == IA64 goto install_finished
-.\bin\ia64\ultradefrag-%ULTRADFGVER%.bin.ia64.exe /S
-if %errorlevel% neq 0 goto fail_inst
-
-:install_finished
+:install_success
 echo Install success!
 goto end
 
-:fail_inst
+:install_fail
 echo Install error!
 goto end_fail
 
@@ -122,12 +111,11 @@ echo Build error (code %ERRORLEVEL%)!
 :end_fail
 set Path=%OLD_PATH%
 set OLD_PATH=
+popd
 exit /B 1
 
 :end
 set Path=%OLD_PATH%
 set OLD_PATH=
+popd
 exit /B 0
-
-:usage
-call build-help.cmd "%~n0"
