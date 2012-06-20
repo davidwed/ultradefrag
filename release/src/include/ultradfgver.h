9fd7beb1d2927b6e59edd22ec37fe60cedc655e4
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2012-06-20 18:20:56 +0000

    [RELEASE] UltraDefrag 5.0.6

--- release/src/include/ultradfgver.h
+++ release/src/include/ultradfgver.h
@@ -1,6 +1,6 @@
-#define VERSION 5,0,5,0 
-#define VERSION2 "5, 0, 5, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 5.0.5" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 5.0.5 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 5.0.5" 
+#define VERSION 5,0,6,0 
+#define VERSION2 "5, 0, 6, 0\0" 
+#define VERSIONINTITLE "UltraDefrag 5.0.6" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 5.0.6 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 5.0.6" 
 #define _UD_HIDE_PREVIEW_ 
