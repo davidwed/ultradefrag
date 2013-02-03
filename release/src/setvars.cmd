47cf033d89ecc387a9b0add69f1a98addab50b20
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-02-03 12:18:01 +0000

    cleanup

--- release/src/setvars.cmd
+++ release/src/setvars.cmd
@@ -1,13 +1,29 @@
 @echo off
 echo Set common environment variables...
-set VERSION=6,0,0,0
-set VERSION2="6, 0, 0, 0\0"
-set ULTRADFGVER=6.0.0
+
+:: UltraDefrag version
+set VERSION_MAJOR=6
+set VERSION_MINOR=0
+set VERSION_REVISION=0
+
+:: alpha1, beta2, rc3, etc.
+:: unset for the final releases
 set RELEASE_STAGE=rc2
-set UDVERSION_SUFFIX=6.0.0-rc2
+
+:: paths to development tools
 set WINDDKBASE=D:\WINDDK\7600.16385.1
 set WINSDKBASE=C:\Program Files\Microsoft SDKs\Windows\v6.1
 set MINGWBASE=D:\Software\MinGWStudio\MinGW
 set MINGWx64BASE=D:\Software\mingw64
 set NSISDIR=D:\Software\Tools\NSIS
 set SEVENZIP_PATH=C:\Program Files\7-Zip
+
+:: auxiliary stuff
+set VERSION=%VERSION_MAJOR%,%VERSION_MINOR%,%VERSION_REVISION%,0
+set VERSION2="%VERSION_MAJOR%, %VERSION_MINOR%, %VERSION_REVISION%, 0\0"
+set ULTRADFGVER=%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_REVISION%
+if "%RELEASE_STAGE%" neq "" (
+    set UDVERSION_SUFFIX=%ULTRADFGVER%-%RELEASE_STAGE%
+) else (
+    set UDVERSION_SUFFIX=%ULTRADFGVER%
+)
