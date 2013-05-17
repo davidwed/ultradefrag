fe8fdc7cf75aaf567cf531e7ac3c922be5069daa
stefanpe <stefanpe@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2013-05-17 08:23:45 +0000

    [RELEASE] UltraDefrag 6.0.2

--- release/src/include/ultradfgver.h
+++ release/src/include/ultradfgver.h
@@ -1,6 +1,6 @@
-#define VERSION 6,0,1,0 
-#define VERSION2 "6, 0, 1, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 6.0.1" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 6.0.1 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 6.0.1" 
+#define VERSION 6,0,2,0 
+#define VERSION2 "6, 0, 2, 0\0" 
+#define VERSIONINTITLE "UltraDefrag 6.0.2" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 6.0.2 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 6.0.2" 
 #define _UD_HIDE_PREVIEW_ 
--- release/src/setvars.cmd
+++ release/src/setvars.cmd
@@ -4,7 +4,7 @@ echo Set common environment variables...
 :: UltraDefrag version
 set VERSION_MAJOR=6
 set VERSION_MINOR=0
-set VERSION_REVISION=1
+set VERSION_REVISION=2
 
 :: alpha1, beta2, rc3, etc.
 :: unset for the final releases
