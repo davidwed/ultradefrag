34bbe75285ee64f062294d027a9dd9e0f037add3
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2014-10-03 14:19:30 +0000

    files prepared for 6.0.3 release

--- release/src/include/ultradfgver.h
+++ release/src/include/ultradfgver.h
@@ -1,6 +1,6 @@
-#define VERSION 6,0,2,0 
-#define VERSION2 "6, 0, 2, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 6.0.2" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 6.0.2 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 6.0.2" 
+#define VERSION 6,0,3,0 
+#define VERSION2 "6, 0, 3, 0\0" 
+#define VERSIONINTITLE "UltraDefrag 6.0.3" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 6.0.3 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 6.0.3" 
 #define _UD_HIDE_PREVIEW_ 
