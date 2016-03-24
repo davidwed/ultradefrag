38d5a8ac2decdd10d729d5a832657da198ae01b4
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2016-03-24 15:26:04 +0000

    x86 compilation reverted back to MinGW use for full compatibility with Windows XP SP1, to keep system requirements simple

--- release/src/build-release.cmd
+++ release/src/build-release.cmd
@@ -30,10 +30,14 @@ mkdir release
 :: build source code package
 call build-src-package.cmd || goto build_failed
 
-:: build all binaries
-call build.cmd --all --use-winsdk || goto build_failed
+:: build 32-bit binaries using MinGW for full compatibility 
+:: with Windows XP SP1, just to keep system requirements simple
+call build.cmd --all --use-mingw || goto build_failed
 copy .\bin\ultradefrag-%UDVERSION_SUFFIX%.bin.i386.exe .\release\
 copy .\bin\ultradefrag-portable-%UDVERSION_SUFFIX%.bin.i386.zip .\release\
+
+:: build 64-bit binaries using Windows SDK
+call build.cmd --all --use-winsdk --no-x86 || goto build_failed
 copy .\bin\amd64\ultradefrag-%UDVERSION_SUFFIX%.bin.amd64.exe .\release\
 copy .\bin\amd64\ultradefrag-portable-%UDVERSION_SUFFIX%.bin.amd64.zip .\release\
 copy .\bin\ia64\ultradefrag-%UDVERSION_SUFFIX%.bin.ia64.exe .\release\
