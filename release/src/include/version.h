a6d781cae859eaa048ef583bc334ad05e59d7b3e
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2016-03-26 13:03:19 +0000

    [RELEASE] 7.0.1

--- release/src/include/version.h
+++ release/src/include/version.h
@@ -1,7 +1,7 @@
-#define VERSION 7,0,0,0 
-#define VERSION2 "7, 0, 0, 0\0" 
-#define VERSIONINTITLE "UltraDefrag 7.0.0" 
-#define VERSIONINTITLE_PORTABLE "UltraDefrag 7.0.0 Portable" 
-#define ABOUT_VERSION "Ultra Defragmenter version 7.0.0" 
-#define wxUD_ABOUT_VERSION "7.0.0" 
+#define VERSION 7,0,1,0 
+#define VERSION2 "7, 0, 1, 0\0" 
+#define VERSIONINTITLE "UltraDefrag 7.0.1" 
+#define VERSIONINTITLE_PORTABLE "UltraDefrag 7.0.1 Portable" 
+#define ABOUT_VERSION "Ultra Defragmenter version 7.0.1" 
+#define wxUD_ABOUT_VERSION "7.0.1" 
 #define _UD_HIDE_PREVIEW_ 
