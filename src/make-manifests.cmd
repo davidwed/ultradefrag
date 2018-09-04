28dae81e1cc214c100d3ef101a30e1c178e9b6e1
dmitriar <dmitriar@297d8b2e-af85-426a-ac7c-16af7146bd1b>
2018-09-04 11:10:36 +0000

    compatibility with Windows 8.1 and Windows 10 improved

--- src/make-manifests.cmd
+++ src/make-manifests.cmd
@@ -62,5 +62,19 @@ rem Example:  call :make_manifest ia64 1.0.0.0 app "Application Name"
     echo     ^<dpiAware^>true^</dpiAware^>
     echo    ^</asmv3:windowsSettings^>
     echo   ^</asmv3:application^>
+    echo   ^<compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1"^>
+    echo    ^<application^>
+    echo     ^<!-- Windows 10 --^>
+    echo     ^<supportedOS Id="{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}"/^>
+    echo     ^<!-- Windows 8.1 --^>
+    echo     ^<supportedOS Id="{1f676c76-80e1-4239-95bb-83d0f6d0da78}"/^>
+    echo     ^<!-- Windows Vista --^>
+    echo     ^<supportedOS Id="{e2011457-1546-43c5-a5fe-008deee3d3f0}"/^>
+    echo     ^<!-- Windows 7 --^>
+    echo     ^<supportedOS Id="{35138b9a-5d96-4fbd-8e2d-a2440225f93a}"/^>
+    echo     ^<!-- Windows 8 --^>
+    echo     ^<supportedOS Id="{4a2f28e3-53b9-4441-ba9c-d69d4a4a6e38}"/^>
+    echo    ^</application^>
+    echo   ^</compatibility^>
     echo ^</assembly^>
 goto :EOF
